e633fb660bc1b86ff45a182bee92081b7a5ee7c5
andrezszambrano <andreskilla2000@gmail.com>
2023-03-20 22:24:51 -0300

    script creado para crear yaml con n clientes

---
+++ create-compose-yaml-n-clients.py
@@ -0,0 +1,45 @@
+import sys
+
+number_of_clients = int(sys.argv[1])
+
+f = open("docker-compose-dev.yaml", "w")
+f.write('''version: '3.9'
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net''')
+
+f.write("\n")
+
+for i in range(1, number_of_clients + 1):
+    f.write('''
+  client{}:
+    container_name: client{}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server'''.format(i, i, i))
+    f.write("\n")
+
+f.write('''
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24''')
+f.write("\n")
+
+f.close()
