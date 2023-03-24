64d249b61fc42c8db4efd3736168e75c26ade22e
andrezszambrano <andreskilla2000@gmail.com>
2023-03-24 13:44:07 -0300

    Se agrega el handler en el cliente para que termine correctamente liberando todos los recursos

--- client/common/client.py
+++ client/common/client.py
@@ -12,34 +12,47 @@ class Client:
         self._port = int(port)
         self._loop_lapse = loop_lapse
         self._loop_period = loop_period
+        self._client_socket = None
+        signal.signal(signal.SIGTERM, __exit_gracefully)
 
-    def handler(self, signum, frame):
+    def __exit_gracefully(self, signum, frame):
+        if self._client_sock != None:
+            self._client_sock.shutdown(socket.SHUT_RDWR)
+            self._client_sock.close()
+            logging.info(f'action: client socket closed | result: success')
+        logging.info(f'action: client shutdowned | result: success')
+        sys.exit(0)
+
+    def __timeout_handler(self, signum, frame):
         logging.info(f"action: timeout_detected | result: success | client_id: {self._id}")
         sys.exit(0)
 
     def run(self):
-        signal.signal(signal.SIGALRM, self.handler)
+        signal.signal(signal.SIGALRM, self.__timeout_handler)
         signal.alarm(self._loop_lapse)
 
         msg_id = 1
         while True:
-            self.connect_and_message_server(msg_id)
+            self.__connect_and_message_server(msg_id)
             msg_id = msg_id + 1
             time.sleep(self._loop_period)
 
-    def connect_and_message_server(self, msg_id):
+    def __connect_and_message_server(self, msg_id):
         try:
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect((self._host, self._port + 23))
-            self.send_message_to_server(client_socket, msg_id)
-            client_socket.close()
+            self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._client_socket.connect((self._host, self._port + 23))
+            self.__send_message_to_server(msg_id)
+
+            self._client_sock.shutdown(socket.SHUT_RDWR)
+            self._client_socket.close()
+            self._client_socket = None
         except OSError as e:
             logging.error(f"action: connect | result: fail | client_id: {self._id} | error: {e}")
 
-    def send_message_to_server(self, client_socket, msg_id):
+    def __send_message_to_server(self, msg_id):
         try:
-            client_socket.send("[CLIENT {}] Message N°{}".format(self._id, msg_id).encode('utf-8'))
-            msg = client_socket.recv(1024).rstrip().decode('utf-8')
+            self._client_socket.send("[CLIENT {}] Message N°{}".format(self._id, msg_id).encode('utf-8'))
+            msg = self._client_socket.recv(1024).rstrip().decode('utf-8')
             logging.info(f"action: receive_message | result: success | client_id: {self._id} | msg: {msg}")
         except OSError as e:
             logging.error(f"action: receive_message | result: fail | client_id: {self._id} | error: {e}")
