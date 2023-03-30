c25cf0bf64273003c568f1605c8196cd8d81ae0e
andrezszambrano <andreskilla2000@gmail.com>
2023-03-30 00:57:02 -0300

    Clase paquete añadida al modelo para evitar multiples sends

--- client/common/client_protocol.py
+++ client/common/client_protocol.py
@@ -1,5 +1,7 @@
-from .protocol import Protocol
+import logging
 
+from .protocol import Protocol
+from .packet import Packet
 
 class ClientProtocol(Protocol):
 
@@ -7,13 +9,17 @@ class ClientProtocol(Protocol):
         super(ClientProtocol, self).__init__()
 
     def send_bet(self, socket, bet):
-        super()._send_byte(socket, super().BET_CHAR)
-        super()._send_n_byte_number(socket, super().ONE_BYTE, bet.agency)
-        super()._send_string(socket, bet.first_name)
-        super()._send_string(socket, bet.last_name)
-        super()._send_n_byte_number(socket, super().FOUR_BYTES, bet.document)
-        super()._send_date(socket, bet.birthdate)
-        super()._send_n_byte_number(socket, super().FOUR_BYTES, bet.number)
+        packet = Packet()
 
+        packet.add_byte(super().BET_CHAR)
+        packet.add_n_byte_number(super().ONE_BYTE, bet.agency)
+        packet.add_string_and_length(bet.first_name)
+        packet.add_string_and_length(bet.last_name)
+        packet.add_n_byte_number(super().FOUR_BYTES, bet.document)
+        packet.add_date(bet.birthdate)
+        packet.add_n_byte_number(super().FOUR_BYTES, bet.number)
+
+        packet.send_to_socket(socket)
+        
     def recv_ok(self, socket):
         super()._recv_byte(socket)
